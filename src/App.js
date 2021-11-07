1a00aa9f3e41ccede14cc28dab7c7ee6aea80a4d
sowmik <mohammad2715>
2021-11-07 11:32:14 +0600

    #Add service components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Services from './components/Services/services';
 import TopBanner from './components/TopBanner/TopBanner';
 function App() {
   return (
     <div>
       <TopBanner/>
+      <Services/>
     </div>
   );
 }
---
+++ src/components/Services/services.jsx
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import designIcon from '../../assest/image/design.png'
+import ecommerceIcon from '../../assest/image/ecommerce.png'
+import webIcon from '../../assest/image/web.png'
+import '../../assest/css/custom.css';
+import '../../assest/css/bootstrap.min.css';
+
+export class services extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container className="text-center">
+
+                    <h1 className="serviceMainTitle">MY SERVICES</h1>
+                    <div className="bottom"></div>
+                    <Row>
+                        <Col lg={4} md={6} sm={12}>
+                           <div className="serviceCard text-center">
+                               <img className="ecommerceIcon" src={ecommerceIcon} alt="" /> 
+                               <h2 className="serviceName">Ecommerce</h2>
+                               <p className="serviceDiscription">I will design and develop ecommerce online store website</p>
+                           </div>
+                        </Col>
+                        <Col lg={4} md={6} sm={12}>
+                           <div className="serviceCard text-center">
+                               <img className="designIcon" src={designIcon} alt=""/>
+                               <h2 className="serviceName">Web Design</h2>
+                               <p className="serviceDiscription">Qualified web design and attractive effects which catches visior's Eye</p>
+                           </div>
+                        </Col>
+                        <Col lg={4} md={6} sm={12}>
+                        <div className="serviceCard text-center">
+                               <img className="webIcon" src={webIcon} alt=""/>
+                               <h2 className="serviceName">Web Development</h2>
+                               <p className="serviceDiscription">Clean and fresh issue free code to make your website dynamic perfectly</p>
+                           </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
+
+export default services
