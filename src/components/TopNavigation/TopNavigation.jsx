42f35c283cd73e7ab0e27762ef75dfef2f172898
sowmik <mohammad2715>
2021-11-07 12:32:42 +0600

    #Change navbar title corol change when scroll in y axis.

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -1,13 +1,33 @@
 import React, { Component, Fragment } from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 export class TopNavigation extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            navBarTitle: "navTitle"
+        }
+    }
+
+    onScroll=()=>{
+        if(window.scrollY>100){
+            this.setState({navBarTitle:'navTitleScrolling'})
+
+        }else if(window.scrollY<100){
+            this.setState({navBarTitle:'navTitle'})
+        }
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.onScroll)
+    }
     render() {
         return (
             <Fragment>
                 <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
                     <Container>
-                    <Navbar.Brand href="#home">EASY LEARNING</Navbar.Brand>
+                    <Navbar.Brand href="#home" className={this.state.navBarTitle}>EASY LEARNING</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
