013a0ca1a8634b46298a52b94686be55f015ce93
sowmik <mohammad2715>
2021-12-02 03:35:33 +0600

    #Added Client review part.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Analysis from './components/Analysis/Analysis';
+//import ClientReview from './components/ClientReview/clientReview';
 import Courses from './components/Courses/Courses';
 import RecentProject from './components/RecentProject/RecentProject';
 import Services from './components/Services/services';
@@ -7,6 +8,7 @@ import Summary from './components/Summary/Summary';
 import TopBanner from './components/TopBanner/TopBanner';
 import TopNavigation from './components/TopNavigation/TopNavigation';
 import Vedios from './components/Vedios/Vedios';
+import ClientReview from './components/ClientReview/ClientReview.jsx';
 function App() {
   return (
     <div>
@@ -18,6 +20,8 @@ function App() {
       <RecentProject/>
       <Courses/>
       <Vedios/>
+      <ClientReview/>
+      <Services />
      
     </div>
   );
